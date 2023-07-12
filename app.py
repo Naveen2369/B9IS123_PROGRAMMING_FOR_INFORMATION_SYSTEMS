a67ff958ce322b1a03c23a236dbdd63ad208761d
Naveen2369 <137410722+Naveen2369@users.noreply.github.com>
2023-07-12 13:21:41 -0700

    created sqlite database
    created sqlite database

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+import sqlite3
 
+DATABASE = 'database.db'
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "programming for information systems ca app"
+def create_tables():
+    conn = sqlite3.connect(DATABASE)
+    conn.close()
+
+@app.route('/create_database')
+def route_create_database():
+    create_tables()
+    return 'Database is created'
 
 if __name__ == '__main__':
+    create_tables()
     app.run()
