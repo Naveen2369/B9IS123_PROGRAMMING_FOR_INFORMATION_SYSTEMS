2a8e2ac782465d0be2f1e1a1ba9a1867260736b4
Chaitanyagurram99 <137643098+Chaitanyagurram99@users.noreply.github.com>
2023-07-16 21:14:02 +0100

    started work on the fronted JavaScript

---
+++ Jazzyard/js_files/main.js
@@ -0,0 +1,24 @@
+/* Json to hold the list of musical instrument information */
+musicalitems =
+    [
+        {
+            "instrumentid": "",
+            "instrumentname": "",
+            "instrumentimage": "",
+            "instrumentprice": ""
+        },
+        {
+
+        }
+    ]
+
+    
+    function GoToHome() {
+        window.location.href = "/";
+    }
+
+
+    $(document).ready(function(){
+
+
+    });
